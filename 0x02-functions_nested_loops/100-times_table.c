e458722da8edc9c4bb7b290fd599b54024c0793f
ArryTec <arrytech@hotmail.com>
2022-12-15 19:02:38 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,37 +6,45 @@
  */
 void print_times_table(int n)
 {
-	int i, a, result;
+	int i, j, k;
 
-	if (n < 0 || n > 15)
-		return;
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		_putchar('0');
-	}
-
-	for (a = 1; a <= n; a++)
-		result = i * a;
-
-	if (result <= 99)
-	{
-		_putchar(' ');
-	}
-	if (result <= 9)
-	{
-		_putchar(' ');
-	}
-	if (result >= 100)
-	{
-		_putchar((result / 100) + '0');
-		_putchar(((result / 10)) % 10 + '0');
-	}
-	else if (result <= 99 && result >= 10)
-	{
-		_putchar((result / 10) + '0');
-	}
-	_putchar((result % 10) + '0');
-	{
-		_putchar('\n');
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				}
+				else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
 	}
 }
